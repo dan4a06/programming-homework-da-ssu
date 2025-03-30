8e0342d4d9e317cf7571d0b86818ad2720f4fae4
dan4a06 <dan4a06@mail.ru>
2025-03-30 23:52:48 +0400

    add task 6

--- secong semester/work with the STL library/work with STL(set, map)/task_6.cpp
+++ secong semester/work with the STL library/work with STL(set, map)/task_6.cpp
@@ -6,33 +6,51 @@
 #include <string>
 using namespace std;
 
-/*
- Дан текст, состоящий из предложений, разделҷнных знаками препинания из
-набора «.?!». Предложения в свою очередь состоят из слов, отделенных друг от
-друга пробелами. Найти слова (без учҷта регистра) и их количество, которые
-встречаются только в повествовательных предложениях.
-*/
-
-string str_lower(string s){
-    for(int i = 0; i < s.length() ; ++i)
-        s[i] = tolower(s[i]);
-    return 0;
+string str_tolower(string s)
+    //функция преобразует строку s в нижний регистр
+    {
+    for (int i = 0; i < s.length(); i++)
+    s[i] = tolower(s[i]);
+    return s;
 }
 
-set <string> set_word(string s) {
+set <string> set_word(string s)
+    //функция возвращает множество из слов нижнего регистра предложения s
+    //Если s="Max and Alex went to play football", то
+    //множество слов set_s = { alex, and, football, max, play, to, went}
+    {
     set <string> set_s;
     if (s[s.length() - 1] != ' ')
-        s += " ";
-    while (s != " "){
-        set_s.insert(str_lower(s.substr(0, s.find(' '))));
+    s += " ";
+    while (s != "") {
+    set_s.insert(str_tolower(s.substr(0, s.find(' '))));
+    s.erase(0, s.find(' ') + 1);
     }
     return set_s;
 }
 
-int main() {
-    //setlocale(LC_ALL, "RU");
-    
-
+int main(){
+    string s, pr;
+    cout << "Input text\n";
+    getline(cin, s);
+    set <string> prres, res, pov, voskl, vopr, s0;
+    //будем составлять множества pov, voskl, vopr из слов
+    //повествовательного, восклицательного
+    //и вопросительного предложений соответственно
+    while (s != "") {
+    int pos = s.find_first_of(".!?");
+    pr = s.substr(0, pos);
+    s0 = set_word(pr);
+    if (s[pos] == '.') pov.insert(s0.begin(), s0.end());
+    if (s[pos] == '!') voskl.insert(s0.begin(), s0.end());
+    if (s[pos] == '?') vopr.insert(s0.begin(), s0.end());
+    s.erase(0, pos + 1);
+    }
+    //находим пересечение множеств pov и vopr
+    set_difference(pov.begin(), pov.end(), vopr.begin(), vopr.end(), inserter(prres, prres.begin()));
+    set_difference(prres.begin(), prres.end(), voskl.begin(), voskl.end(), inserter(res, res.begin()));
+    for (set <string> ::iterator i = res.begin(); i != res.end(); i++)
+    cout << *i << " ";
 
     return 0;
 }
